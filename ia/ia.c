a25a39115515a5721ad1db97c38eda7ae4468ead
Micrified <charles.xrandolph2@gmail.com>
2019-01-17 23:24:41 +0100

    Added cmp to arthmetic operations

--- ia/ia.c
+++ ia/ia.c
@@ -46,7 +46,7 @@ const char *inst_cnd[] = {"jo","jno","js","jns","je","jz","jne","jnz","jb",
 
 // All tracked ALU instructions (all arithmetic/bit operations).
 const char *inst_alu[] = {"ror", "rol", "shl", "sal", "shr", "sar", "not", 
-	"and", "or", "xor", "neg", "sub", "add", "idiv", NULL};
+	"and", "or", "xor", "neg", "sub", "add", "idiv", "cmp", NULL};
 
 // All tracked multiplication instructions.
 const char *inst_mpy[] = {"imul", NULL};
